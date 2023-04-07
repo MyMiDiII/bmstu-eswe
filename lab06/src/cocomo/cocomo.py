db9a848ad2942bc758687c487d7067be0a7c6782
MyMiDiII <mymidi.student@yandex.ru>
2023-04-07 21:50:56 +0300

    ADD: лр6 синхронизация combo- и spinbox-ов; константы и базовые формулы cocomo

---
+++ lab06/src/cocomo/cocomo.py
@@ -0,0 +1,49 @@
+import math
+
+from constants import *
+
+class Cocomo:
+    def __init__(self, kloc, mode):
+        self.drivers = {
+            Driver.RELY : DriversCoeffs.get_coef(Driver.RELY),
+            Driver.DATA : DriversCoeffs.get_coef(Driver.DATA),
+            Driver.CPLX : DriversCoeffs.get_coef(Driver.CPLX),
+
+            Driver.TIME : DriversCoeffs.get_coef(Driver.TIME),
+            Driver.STOR : DriversCoeffs.get_coef(Driver.STOR),
+            Driver.VIRT : DriversCoeffs.get_coef(Driver.VIRT),
+            Driver.TURN : DriversCoeffs.get_coef(Driver.TURN),
+
+            Driver.MODP : DriversCoeffs.get_coef(Driver.MODP),
+            Driver.TOOL : DriversCoeffs.get_coef(Driver.TOOL),
+            Driver.SCED : DriversCoeffs.get_coef(Driver.SCED),
+
+            Driver.ACAP : DriversCoeffs.get_coef(Driver.ACAP),
+            Driver.AEXP : DriversCoeffs.get_coef(Driver.AEXP),
+            Driver.PCAP : DriversCoeffs.get_coef(Driver.PCAP),
+            Driver.VEXP : DriversCoeffs.get_coef(Driver.VEXP),
+            Driver.LEXP : DriversCoeffs.get_coef(Driver.LEXP)
+        }
+
+        self.size = kloc
+        self.mode = MODES[mode]
+
+    @property
+    def eaf(self):
+        return math.prod(self.drivers.values())
+
+    @property
+    def effort_base(self):
+        return self.mode.c1 * self.eaf * self.size ** self.mode.p1
+
+    @property
+    def time_base(self):
+        return self.mode.c2 * self.effort_base ** self.mode.p2
+
+    def get_results(self):
+        return {
+                "eaf" : self.eaf,
+                "effort_base" : self.effort_base,
+                "time_base" : self.time_base
+                }
+
--- lab06/src/main.py
+++ lab06/src/main.py
@@ -1,7 +1,11 @@
 import sys
 
 from PyQt5 import QtWidgets
-from mainwindow import Ui_MainWindow
+from gui.mainwindow import Ui_MainWindow
+
+from cocomo.constants import *
+
+import time
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -10,10 +14,105 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.drivers_comboxes = [
+                self.ui.cbRELY,
+                self.ui.cbDATA,
+                self.ui.cbCPLX,
+
+                self.ui.cbTIME,
+                self.ui.cbSTOR,
+                self.ui.cbVIRT,
+                self.ui.cbTURN,
+
+                self.ui.cbMODP,
+                self.ui.cbTOOL,
+                self.ui.cbSCED,
+
+                self.ui.cbACAP,
+                self.ui.cbAEXP,
+                self.ui.cbPCAP,
+                self.ui.cbVEXP,
+                self.ui.cbLEXP
+                ]
+
+        self.drivers_spinboxes = [
+                self.ui.dsbRELY,
+                self.ui.dsbDATA,
+                self.ui.dsbCPLX,
+
+                self.ui.dsbTIME,
+                self.ui.dsbSTOR,
+                self.ui.dsbVIRT,
+                self.ui.dsbTURN,
+
+                self.ui.dsbMODP,
+                self.ui.dsbTOOL,
+                self.ui.dsbSCED,
+
+                self.ui.dsbACAP,
+                self.ui.dsbAEXP,
+                self.ui.dsbPCAP,
+                self.ui.dsbVEXP,
+                self.ui.dsbLEXP
+                ]
+
+        self.__init_drivers_comboxes()
+
+        self.ui.cbRELY.currentIndexChanged.connect(self.check)
+
+    def check(self):
+        print("check")
+        print(self.ui.cbRELY)
+        print(self.ui.cbRELY.currentIndex())
+
+    def __init_drivers_comboxes(self):
+        baseText = [
+                "Очень низкий",
+                "Низкий",
+                "Номинальный",
+                "Высокий",
+                "Очень высокий",
+                ]
+        cbItems = [
+                [baseText,      2],
+                [baseText[1:],  1],
+                [baseText,      2],
+
+                [baseText[2:],  0],
+                [baseText[2:],  0],
+                [baseText[1:],  1],
+                [baseText[1:],  1],
+
+                [baseText,      2],
+                [baseText,      2],
+                [baseText,      2],
+
+                [baseText,      2],
+                [baseText,      2],
+                [baseText,      2],
+                [baseText[:-1], 2],
+                [baseText[:-1], 2]
+                ]
+
+
+        for i, combox in enumerate(self.drivers_comboxes):
+            combox.addItems(cbItems[i][0])
+            f = lambda y, x=i: self.syncDriverSB(x,
+                       self.drivers_comboxes[x],
+                       self.drivers_spinboxes[x],
+                       Driver(x),
+                       cbItems[x][1])
+            combox.currentIndexChanged.connect(f)
+            combox.setCurrentIndex(cbItems[i][1])
+
+
+    def syncDriverSB(self, x, combox, spinbox, driver, nominal):
+        index = combox.currentIndex()
+        spinbox.setValue(DriversCoeffs.get_coef(driver,
+                                                Level(2 - nominal + index)))
+
 
 def main():
-    from PyQt5.QtWidgets import QStyleFactory
-    print(QStyleFactory.keys())
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle("Fusion")
     main = MainWindow()
